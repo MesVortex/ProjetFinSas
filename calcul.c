23c0e0236d7b99943886fb373cfb783e32b5cea2
MesVortex <meskinemostafa4@gmail.com>
2023-09-13 12:57:29 +0200

    calcul almost done

--- calcul.c
+++ calcul.c
@@ -7,26 +7,27 @@ float divise(float a,float b);
 float subs(float a,float b);
 
 int main(){
-    float a,b;
+    float a,b,i;
     char c;
     printf("donner le 1er nbr:");
     scanf("%f", &a);
     printf("donner le 2eme nbr:");
     scanf("%f", &b);
-    printf("donner l\'operation:");
-    scanf("%c", c);
+    printf("donner l\'operation:\n");
+    scanf("%s", &c);
     again:
         switch(c){
-            case '+': add(a,b);
+            case '+': i=add(a,b);
                 break;
-            case '-': subs(a,b);
+            case '-': i=subs(a,b);
                 break;
-            case '*': multiply(a,b);
+            case '*': i=multiply(a,b);
                 break;
-            case '/': divise(a,b);
+            case '/': i=divise(a,b);
                 break;
-            default : printf("svp donne une operation valid! (exp:'+' '-' '*' '/')");   
+            default : printf("svp donne une operation valid! (exp:'+' '-' '*' '/')");
     }
+    printf("le resultat est: %.2f \n",i);
     return 0;
 }
 float add(float a,float b){
