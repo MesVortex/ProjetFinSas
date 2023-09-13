8bc7fee21111360058d181c3df946762f44abdc7
MesVortex <meskinemostafa4@gmail.com>
2023-09-13 17:56:06 +0200

    kalkil 2.0

---
+++ kalkil.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float add(float a,float b);
+float multiply(float a,float b);
+float divise(float a,float b);
+float subs(float a,float b);
+
+int main(){
+    float a,b,i;
+    char c;
+    printf("donner le 1er nbr:");
+    scanf("%f", &a);
+    printf("donner le 2eme nbr:");
+    scanf("%f", &b);
+    printf("donner l\'operatio#include <stdio.h>
+#include <stdlib.h>
+
+float add(float a,float b);
+float multiply(float a,float b);
+float divise(float a,float b);
+float subs(float a,float b);
+
+int main(){
+    float a,b,i;
+    char c;
+    printf("donner le 1er nbr:");
+    scanf("%f", &a);
+    printf("donner le 2eme nbr:");
+    scanf("%f", &b);
+    printf("donner l\'operation:");
+    scanf("%s", c);
+    while ((c =! '+') || (c =! '*') || (c =! '-') || (c =! '/') ){
+        printf("svp donne une operation valid! (exp:'+' '-' '*' '/')");
+        scanf("%s", c);
+    }
+    switch(c){
+        case '+': i=add(a,b);
+            break;
+        case '-': i=subs(a,b);
+            break;
+        case '*': i=multiply(a,b);
+            break;
+        case '/': i=divise(a,b);
+            break;
+    }
+    printf("le resultat est : %.2f", i);
+    return 0;
+}
+float add(float a,float b){
+        return (a+b);
+}
+float multiply(float a,float b){
+        return (a*b);
+}
+float subs(float a,float b){
+        return (a-b);
+}
+float divise(float a,float b){
+        return (a/b);
+}n:");
+    scanf("%s", &c);
+    while ((c =! '+') || (c =! '*') || (c =! '-') || (c =! '/') ){
+        printf("svp donner une operation valid! (exp:'+' '-' '*' '/')");
+        scanf("%s", &c);
+    }
+    switch(c){
+        case '+': i=add(a,b);
+            break;
+        case '-': i=subs(a,b);
+            break;
+        case '*': i=multiply(a,b);
+            break;
+        case '/': i=divise(a,b);
+            break;
+    }
+    printf("le resultat est : %.2f", i);
+    return 0;
+}
+float add(float a,float b){
+        return (a+b);
+}
+float multiply(float a,float b){
+        return (a*b);
+}
+float subs(float a,float b){
+        return (a-b);
+}
+float divise(float a,float b){
+        return (a/b);
+}
