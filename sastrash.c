5dad69d16fdf058baa3cf71863040c7f7527b56a
MesVortex <meskinemostafa4@gmail.com>
2023-09-21 00:01:40 +0100

    projet sas update from home

--- sastrash.c
+++ sastrash.c
@@ -15,20 +15,37 @@ typedef struct{
     deadline de_li;
 }ToDo;
 
-int G_ID = 1001;
+int indice, G_ID = 1001;
 ToDo task[200];
 
-int add(int i);
-int add_more(int indice);
+void add();
+void add_more();
 void show(int a);
-void swap(ToDo task[], int a, int b);
+void swap( int a, int b);
 //void modify();
 
 int main(){
-    int i,a;
-    i = add(i);
-    show(i-1);
-    i = add_more(i);
+    int choix;
+    again:
+    printf("choix");
+    scanf("%d", &choix);
+    switch (choix){
+    case 1:  printf("%d\n",indice);
+        add();
+        printf("%d\n",indice);
+        goto again;
+        break;
+    case 2: printf("%d\n",indice);
+        add_more();
+        printf("%d\n", indice);
+        break;
+    case 3: for (int i = 0; i < indice; i++){show(i);}
+        break;
+    case 4: // void modify
+        break;
+    default:
+        break;
+    }
     return 0;
 }
 
@@ -40,45 +57,63 @@ void show(int a){
         printf("statut: %s\n\n", task[a].status);
     }
 
-int add(int i){
-    char yes_no;
+void add(){
+
     printf("Entrez le nom de la tache:");
-    scanf("%s", task[i].titre);
+    scanf("%s", task[indice].titre);
 
-    printf("voulez vous ajouter une decription? [y/n] :");
-    scanf("%s", &yes_no);
-
-    if (yes_no == 'y'){
-        printf("donner une decription (150 charactere max):");
-        scanf(" %[^\n]", task[i].descri);
-    }
+    printf("donner une decription (optional & 150 charactere max):");
+    scanf(" %[^\n]", task[indice].descri);
 
     printf("ajouter une deadline (JJ/MM/AA):");
-    scanf("%d/%d/%d", &task[i].de_li.jour, &task[i].de_li.mois, &task[i].de_li.annee);
+    scanf("%d/%d/%d", &task[indice].de_li.jour, &task[indice].de_li.mois, &task[indice].de_li.annee);
     
     printf("entrer le statut de votre tache (todo, doing, done):");
-    scanf("%s", task[i].status);
+    scanf("%s", task[indice].status);
 
-    task[i].ID = G_ID;
+    task[indice].ID = G_ID;
     printf("tache ajouté avec succès!\n (ID de ce tache: %d)\n", G_ID);
     G_ID++;
-    i++;
-    return i;
+    indice++;
 }
 
-int add_more(int indice){
-    int a,b;
+void add_more(){
+    int a,i,b = indice;
     printf("combien de tache voulez vous ajoutez? :");
     scanf("%d", &a);
-    for (int i = indice; i < indice + a; i++){
-        b = add(i);
+    for ( i = b; i < b + a ; i++){
+        add();
     }
-    return b;
 }
 
-void swap(ToDo task[],int i,int j){
+void swap( int i, int j){
     ToDo temp;
     temp.ID = task[i].ID;
-    task[i].ID;
+    task[i].ID = task[j].ID;
+    task[j].ID = temp.ID;
+    
+    temp.de_li.jour = task[i].de_li.jour;
+    task[i].de_li.jour = task[j].de_li.jour;
+    task[j].de_li.jour = temp.de_li.jour;
+
+    temp.de_li.mois = task[i].de_li.mois;
+    task[i].de_li.mois = task[j].de_li.mois;
+    task[j].de_li.mois = temp.de_li.mois;
+
+    temp.de_li.annee = task[i].de_li.annee;
+    task[i].de_li.annee = task[j].de_li.annee;
+    task[j].de_li.annee = temp.de_li.annee;
+
+    strcpy(temp.titre,task[i].titre);
+    strcpy(task[i].titre,task[j].titre);
+    strcpy(task[j].titre,temp.titre);
+
+    strcpy(temp.descri,task[i].descri);
+    strcpy(task[i].descri,task[j].descri);
+    strcpy(task[j].descri,temp.descri);
+
+    strcpy(temp.status,task[i].status);
+    strcpy(task[i].status,task[j].status);
+    strcpy(task[j].status,temp.status);
 }
 //void modify(ToDo task[]){}
